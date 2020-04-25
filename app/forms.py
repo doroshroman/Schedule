c5bbe96b5d15ad0aa6245644b3c01e2af51e3d8c
Roman <doroshroman27@gmail.com>
2020-04-25 16:02:05 +0300

    added form validation

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,6 @@ from wtforms.validators import DataRequired
 
 class AddScheduleForm(FlaskForm):
     groupname = StringField('Group Name', validators=[DataRequired()])
-    date_from = DateField('Date From', format='%d/%m/%Y', validators=[DataRequired()])
-    date_to = DateField('Date To', format='%d/%m/%Y', validators=[DataRequired()])
+    date_from = DateField('Date From', format='%d.%m.%Y', validators=[DataRequired()])
+    date_to = DateField('Date To', format='%d.%m.%Y', validators=[DataRequired()])
     submit = SubmitField("Create Schedule")
--- app/routes.py
+++ app/routes.py
@@ -10,9 +10,12 @@ def hello_world():
 @app.route('/add_schedule', methods=['GET', 'POST'])
 def add_schedule():
     form = AddScheduleForm()
+    groupname = None
+    date_from = None
+    date_to = None
     if form.validate_on_submit():
         # Data from form is ready
-        pass
-
-
-    return render_template('add_schedule.html', form=form)
+        groupname = form.groupname.data
+        date_from = form.date_from.data
+        date_to = form.date_to.data
+    return render_template('add_schedule.html', form=form, group=groupname, date_from=date_from, date_to=date_to)
