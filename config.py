ed94e7bfa8554d5db85ba4001ae58c686954c62b
Nazar Shcherbiy <Chay23@users.noreply.github.com>
2020-04-27 18:29:52 +0300

    Delete app.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class Config():
     # To protect form from attack
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'so-secret'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://root:Chaii2323@localhost//mydb'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+mysqlconnector://root:root@localhost/mydb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
